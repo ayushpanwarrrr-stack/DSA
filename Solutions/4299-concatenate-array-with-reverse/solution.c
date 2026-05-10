9de184ad11c4413bbbcab4b679c9a4641647a1df
Ayush Panwar <ayushpanwarrrr@gmail.com>
2026-05-10 02:43:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 16.1 MB (41.83%)

---
+++ Solutions/4299-concatenate-array-with-reverse/solution.c
@@ -0,0 +1,14 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* concatWithReverse(int* nums, int numsSize, int* returnSize) {
+    *returnSize=2*numsSize;
+    int* ans=(int*)malloc((*returnSize)*sizeof(int));
+    for(int i=0;i<numsSize;i++){
+        ans[i]=nums[i];
+        ans[i+numsSize]=nums[numsSize-i-1];
+    }
+     
+    
+    return ans;
+}
