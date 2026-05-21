3a432bb90c15c588ba1a27e3b4b279eb18bfedcc
Ayush Panwar <ayushpanwarrrr@gmail.com>
2026-05-21 15:03:06 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 8.6 MB (59.75%)

---
+++ Solutions/0953-reverse-only-letters/solution.c
@@ -0,0 +1,19 @@
+char* reverseOnlyLetters(char* s) {
+    int i = 0;
+    int j = strlen(s) - 1;
+    while (i < j) {
+        if (!isalpha(s[i])){
+            i++;
+        } else if (!isalpha(s[j])){
+            j--;
+        } else {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    return s;
+}
+
