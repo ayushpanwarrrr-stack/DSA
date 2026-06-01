c0667f64c87a0ae5f0248ad03733b2db1608dbcf
Ayush Panwar <ayushpanwarrrr@gmail.com>
2026-06-01 03:24:53 +0000

    Sync LeetCode submission Runtime - 27 ms (76.71%), Memory - 106.8 MB (16.30%)

---
+++ Solutions/2245-destroying-asteroids/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        long long mass1=mass;
+        sort(asteroids.begin() , asteroids.end());
+        for(int i=0; i<asteroids.size();i++){
+            if(mass1<asteroids[i])
+            return false;
+            else 
+            mass1+=asteroids[i];
+        }
+        return true;
+    }
+};
